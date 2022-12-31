5a5981a27a62ec9a5c223b9b76e4f1d713d8d9b0
Korir Moses <korirkipsmoze@gmail.com>
2022-12-31 13:50:33 +0300

    saving data to data base

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+//libraries
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 8000;
+
+//models
+const user = require("./model/user");
+const User = require("./model/user");
 
 //Mongodb connection;
 const database_url = "mongodb://localhost:27017/node";
@@ -28,8 +33,36 @@ app.post("/", (req, res) => {
   let response;
 
   if (text === "") {
-    response = "CON Main menu";
-  } else if (text !== "") {
+    console.log("1");
+    response = "CON Enter Your Name";
+  }
+  if (text !== "") {
+    let array = text.split("*");
+    if (array.length === 1) {
+      response = "CON Enter Your ID Number";
+    } else if (array.length === 2) {
+      if (parseInt(array[1]) > 0) {
+        response =
+          "CON Confirm If you want to save the data\n1.Confirm.\n 2.Cancel";
+      } else {
+        response = "END Network error please try again later";
+      }
+    } else if (array.length === 3) {
+      if (parseInt(array[2]) === 1) {
+        let data = new User();
+        data.fullname = array[0];
+        data.id_number = array[1];
+        data.save(() => {
+          response = "END Your data has been saved";
+        });
+      } else if (parseInt(array[2]) === 2) {
+        response = "END Data was not saved";
+      } else {
+        response = "END Inavlid input";
+      }
+    } else {
+      response = "END Network error please try again later";
+    }
   }
 
   setTimeout(() => {
