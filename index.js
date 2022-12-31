9b46f3157b7f5e199a289d69cd82b02d637c5df2
Korir Moses <korirkipsmoze@gmail.com>
2022-12-31 17:03:51 +0300

    completed one entry

--- index.js
+++ index.js
@@ -33,35 +33,35 @@ app.post("/", (req, res) => {
   let response;
 
   if (text === "") {
-    console.log("1");
-    response = "CON Enter Your Name";
+    response = "CON Main menu\n";
+    response += "1. Register To acess Loan\n";
   }
-  if (text !== "") {
+  if (text == "1") {
+    response = "CON Enter Your Full Name";
+  } else if (text !== "") {
     let array = text.split("*");
-    if (array.length === 1) {
-      response = "CON Enter Your ID Number";
-    } else if (array.length === 2) {
-      if (parseInt(array[1]) > 0) {
-        response =
-          "CON Confirm If you want to save the data\n1.Confirm.\n 2.Cancel";
-      } else {
-        response = "END Network error please try again later";
-      }
+    if (array.length === 2) {
+      response = "CON Enter your ID";
     } else if (array.length === 3) {
-      if (parseInt(array[2]) === 1) {
-        let data = new User();
-        data.fullname = array[0];
-        data.id_number = array[1];
-        data.save(() => {
-          response = "END Your data has been saved";
-        });
-      } else if (parseInt(array[2]) === 2) {
-        response = "END Data was not saved";
-      } else {
-        response = "END Inavlid input";
+      response = "CON Main menu\n";
+      response += "1. Borrow a loan\n";
+      response += "2. Check Loan Limit\n";
+      response += "3. Pay loan\n";
+    } else if (array.length === 4) {
+      if (parseInt(array[3]) == 1) {
+        response = "CON View loan options\n";
+        response += "1. Short-term loan: 5% interest, due in 30 days\n";
+        response += "2. Medium-term loan: 10% interest, due in 90 days\n";
+        response += "3. Long-term loan: 15% interest, due in 180 days\n";
       }
-    } else {
-      response = "END Network error please try again later";
+    } else if (array.length === 5) {
+      if (parseInt(array[4]) > 0) {
+        response = "CON Apply for a loan\n";
+        response += "Enter loan amount: [user inputs loan amount]\n";
+      }
+    } else if (array.length === 6) {
+      response =
+        "CON Confirm If you want apply for the loan\n1.Confirm.\n 2.Cancel";
     }
   }
 
